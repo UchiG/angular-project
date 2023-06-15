ad4dea089dc44af143f09e01a088ff77d39ea882
Uchirai <uchirai.govere@gmail.com>
2023-06-15 11:30:21 +0200

    using ViewChild template querying mechanism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
+import { Component, ViewChild } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,11 @@ export class AppComponent {
 
     courses = COURSES;
 
-    title = COURSES[0].description;
-
-    price = 9.99;
-
-    rate = 0.67;
-
-    course = COURSES[0];
-
-    startDate = new Date(2000, 0, 1);
+    @ViewChild(CourseCardComponent)
+    card: CourseCardComponent;
 
     onCourseSelected(course:Course) {
-      console.log("app component - click event bubbled...", course);
+      console.log(this);
     }
 
 }
