84a1076b0274969585eecc7e9b861f38255b9567
Uchirai <uchirai.govere@gmail.com>
2023-06-15 11:02:49 +0200

    created course variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,12 @@ export class AppComponent {
 
     title = COURSES[0].description;
 
+    price = 9.99;
+
+    rate = 0.67;
+
+    course = COURSES[0];
+
     startDate = new Date(2000, 0, 1);
 
     onCourseSelected(course:Course) {
