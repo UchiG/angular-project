5a93215486a1d28c0f730ac5a2fff9bc22262b99
Uchirai <uchirai.govere@gmail.com>
2023-06-17 21:09:18 +0200

    implementing AfterViewInit lifecycle hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
