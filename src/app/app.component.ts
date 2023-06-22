dc19714fcc567a7d39ddd500493e30ff7c6e899d
Uchirai <uchirai.govere@gmail.com>
2023-06-22 07:43:24 +0200

    fixed import statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewChild, ElementRef, QueryList } from '@angular/core';
+import { Component, ViewChild, ElementRef, QueryList, ViewChildren} from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { AfterViewInit } from '@angular/core';
-import { viewChildren } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +13,7 @@ export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
-  @viewChildren(CourseCardComponent)
+  @ViewChildren(CourseCardComponent)
   cards: QueryList<CourseCardComponent>;
 
   constructor() {
