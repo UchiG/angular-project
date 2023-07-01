c86f0687b34737b07f38d9be12e0f663e87dfbeb
Uchirai <uchirai.govere@gmail.com>
2023-07-01 19:17:02 +0200

    imported course card component in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 import { AppComponent } from './app.component';
 
