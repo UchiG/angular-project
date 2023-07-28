484ad0a88cbdd87ba75de6f609fcc4bbe4ab677c
Uchirai <uchirai.govere@gmail.com>
2023-07-28 20:23:47 +0200

    adjusted spacing on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, ViewChild, AfterViewInit, ElementRef, QueryList, ViewChildren} from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -6,35 +13,32 @@ import { CourseCardComponent } from './course-card/course-card.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit {
+  courses = COURSES;
 
-    courses = COURSES;
-
-  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
   cards: QueryList<ElementRef>;
 
-  constructor() {
-
-  }
+  constructor() {}
 
   ngAfterViewInit() {
     console.log(this.cards);
   }
 
-    onCourseSelected(course:Course) {
+  onCourseSelected(course: Course) {}
 
-    }
-
-    onCourseEdited() {
-      this.courses.push({
-        id: 1,
-        description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
-        category: 'INTERMEDIATE',
-        lessonsCount: 10
-    })
-    }
+  onCourseEdited() {
+    this.courses.push({
+      id: 1,
+      description: 'Angular Core Deep Dive',
+      iconUrl:
+        'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+      longDescription:
+        'A detailed walk-through of the most important part of Angular - the Core and Common modules',
+      category: 'INTERMEDIATE',
+      lessonsCount: 10,
+    });
+  }
 }
